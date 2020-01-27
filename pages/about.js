d9c6b0fcbe81cec337204a790cc0cc1884aa2ea9
John Alex Jacob <johnalexjacob@gmail.com>
2020-01-28 01:01:32 +0200

    how do I do this

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <div>
       <h1>Modified About Page</h1>
-      <p>This has a bit more content</p>
+      <p>This has a bit more content.</p>
       <img src="https://source.unsplash.com/random/800x600" alt="Unsplash" />
     </div>
   )

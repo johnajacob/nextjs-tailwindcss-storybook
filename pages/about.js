56484369184114584e0f569f04fc5585d9561a1d
John Alex Jacob <johnalexjacob@gmail.com>
2020-01-28 00:51:25 +0200

    extra testing

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,9 @@
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>Modified About Page</h1>
+      <p>This has a bit more content</p>
+      <img src="https://source.unsplash.com/random/800x600" alt="Unsplash" />
     </div>
   )
 }
